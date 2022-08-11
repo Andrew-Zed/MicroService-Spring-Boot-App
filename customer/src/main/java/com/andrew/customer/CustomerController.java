a193fc18b42e3c24fd3d4f4650ad9f35f34a2505
Andrew-Zed <andrew.awadyke@gmail.com>
2022-08-11 18:40:31 +0100

    microservice communication via HTTP implemented

--- customer/src/main/java/com/andrew/customer/CustomerController.java
+++ customer/src/main/java/com/andrew/customer/CustomerController.java
@@ -1,5 +1,6 @@
 package com.andrew.customer;
 
+import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -9,9 +10,9 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("api/v1/customer")
-public record CustomerController(CustomerService customerService) {
-
-
+@AllArgsConstructor
+public class CustomerController {
+    private final CustomerService customerService;
     @PostMapping
     public void registerCustomer(@RequestBody CustomerRegistrationRequest customerRegistrationRequest) {
         log.info("new customer registration {}", customerRegistrationRequest);
---
+++ fraud/src/main/java/com/andrew/fraud/FraudCheckResponse.java
@@ -0,0 +1,4 @@
+package com.andrew.fraud;
+
+public record FraudCheckResponse(Boolean isFraudster) {
+}
