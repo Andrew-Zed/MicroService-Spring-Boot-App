457e329f09790d59e804986b05e9123169de6dfc
Andrew-Zed <andrew.awadyke@gmail.com>
2022-08-11 05:37:13 +0100

    first microservice implemented

--- customer/src/main/java/com/andrew/customer/CustomerService.java
+++ customer/src/main/java/com/andrew/customer/CustomerService.java
@@ -3,17 +3,17 @@ package com.andrew.customer;
 import org.springframework.stereotype.Service;
 
 @Service
-public record CustomerService() {
+public record CustomerService(CustomerRepository customerRepository) {
     public void registerCustomer(CustomerRegistrationRequest request) {
         Customer customer = Customer.builder()
-                .firstName(request.firstName())
+                .firstName(request  .firstName())
                 .lastName(request.lastName())
                 .email(request.email())
                 .build();
 
         // todo: check if email is valid
         // todo: check if email is not taken
-        // todo: store customer in db
+        customerRepository.save(customer);
     }
 
 }
